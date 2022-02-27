b3cd66dedf33dfee9fc2ae46d3894ab3d4f9a6a4
Akash Srivastava <akashniki@gmail.com>
2022-02-27 17:22:05 +0530

    picasso: Switch to QTI perf component

--- device.mk
+++ device.mk
@@ -407,6 +407,10 @@ PRODUCT_COPY_FILES += \
 # RenderScript
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
+    
+# QTI
+TARGET_COMMON_QTI_COMPONENTS := \
+    perf
 
 # RIL
 PRODUCT_PACKAGES += \
