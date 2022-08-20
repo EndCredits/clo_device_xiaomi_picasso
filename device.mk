d72bf707219e1f838fc8295fa5ec4dccc44c1c9e
EndCredits <alicization.han@gmail.com>
2022-08-20 22:05:33 +0800

    picasso: Let's go. 404 Bug Not Found!
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- device.mk
+++ device.mk
@@ -1,8 +1,18 @@
 #
 # Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2022 Project 404
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
@@ -22,7 +32,8 @@ PRODUCT_PACKAGES += \
     FrameworkResOverlayPicasso \
     SettingsOverlayPicasso \
     SystemUIOverlayPicasso \
-    TelephonyOverlayPicasso
+    TelephonyOverlayPicasso \
+    WifiResCommon
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
@@ -471,7 +482,6 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     vendor.qti.hardware.wifi.hostapd@1.2.vendor \
     vendor.qti.hardware.wifi.supplicant@2.2.vendor \
-    WifiResCommon \
     wpa_supplicant \
     wpa_supplicant.conf
 
--- p404_picasso.mk
+++ p404_picasso.mk
@@ -11,15 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from picasso device
 $(call inherit-product, device/xiaomi/picasso/device.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-TARGET_USES_AOSP_RECOVERY := true
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
-TARGET_SUPPORTS_QUICK_TAP := true
+# Inherit some common 404 stuff.
+$(call inherit-product, vendor/404/configs/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_picasso
+PRODUCT_NAME := p404_picasso
 PRODUCT_DEVICE := picasso
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30 5G
